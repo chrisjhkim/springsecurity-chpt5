ce02fa6e418612f1af6fb47642977507915d6308
김준현 <chris1108@dreamsecurity.com>
2023-11-23 08:50:46 +0900

    5.2.1 보안 컨텍스트를 위한 보유 전략 이용

--- src/main/java/com/example/springsecurity/chpt5/controller/HelloController.java
+++ src/main/java/com/example/springsecurity/chpt5/controller/HelloController.java
@@ -1,20 +1,29 @@
 package com.example.springsecurity.chpt5.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class HelloController {
 
-
-
 	@GetMapping("/hello")
-	public String hello(){
+	public String hello(Authentication authentication){
 		log.info("HelloController.hello");
-		return "hello";
+		return "Hello, " + authentication.getName();
+	}
+
+	@GetMapping("/hello2")
+	public String hello2(){
+		log.info("HelloController.hello2");
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+
+		return "Hello, " + authentication.getName();
 	}
 
 }
